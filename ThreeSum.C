9ab5bd322171c5b3f08b377444f62da1e2b23a87
fritzonwilson <161024973+fritzonwilson@users.noreply.github.com>
2025-01-31 23:31:20 +0530

    Update ThreeSum.C
    The earlier solution printed solutions where the three elements where printed repeatedly in a different order. This new solution takes care of that issue by removing all duplicate elements in the initial array before finding all sets of three elements in the array that equal to 0.

--- ThreeSum.C
+++ ThreeSum.C
@@ -8,6 +8,21 @@ int main()
     for(int i =0;i<n;i++){
         scanf("%d",&arr[i]);
     }
+    
+    
+        for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {  
+                for (int k = j; k < n - 1; k++) {
+                    arr[k] = arr[k + 1];
+                }
+                n--;  
+                j--;  
+            }
+        }
+    }
+    
+    
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             for(int k=j+1;k<n;k++){
@@ -15,8 +30,8 @@ int main()
                     printf("%d %d %d\n",arr[i],arr[j],arr[k]);
                 }
             }
-        }
-    }
+       }
+}
 
-    
+
 }
