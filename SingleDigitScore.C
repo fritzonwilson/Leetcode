b0aa9f4c0915775e05a648ba2ef6445c23eda43f
fritzonwilson <161024973+fritzonwilson@users.noreply.github.com>
2025-02-02 23:40:16 +0530

    Update SingleDigitScore.C
    This solution only requires the user to find the modulus 9 of a number to get the single digit score reducing the time complexity to 0(1).

--- SingleDigitScore.C
+++ SingleDigitScore.C
@@ -1,23 +1,5 @@
-#include <stdio.h>
-int main() {
-    int n;
-    int sum=0,sum1=0,sum2=0;
-    int mod;
-    int divi;
-    printf("ENTER THE NUMBER: ");
-    scanf("%d",&n);
-    while(n!=0){
-        mod=n%10;
-        n=n/10;
-        sum=sum+mod;
-        
-    }
-    while(sum!=0){
-        mod=sum%10;
-        sum=sum/10;
-        sum1=sum1+mod;
-        
-    }
-    printf("The Single Digit Score is : %d",sum1);
-    return 0;
-}
+num=int(input())
+if num%9==0:
+    print(9)
+else:
+    print(num%9)
